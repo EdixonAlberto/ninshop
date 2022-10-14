6ca9a31f37fe5f3d58a2692831404e442ab5bb1f
Edixon Piña <edixonalbertto@gmail.com>
2022-10-14 10:45:42 -0400

    install libraries, modify template and style in view App
    - add library nintendo-switch-eshop
    - add library querystring

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import { getGamesAmerica, GameUS } from 'nintendo-switch-eshop'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [games, setGames] = useState<GameUS[]>([])
+
+  async function getGames(): Promise<void> {
+    const response = await getGamesAmerica()
+    const date = new Date()
+    const games = response.filter((game) => {
+      return game.platform == 'Nintendo Switch' && new Date(game.releaseDateDisplay).getFullYear() == date.getFullYear()
+    })
+
+    setGames(games)
+  }
+
+  function formatPrice(price: number | null): string {
+    if (price) {
+      let priceStr = price.toString()
+      const qtyDecimal = priceStr.search(/\./) > -1 ? priceStr.split('.')[1].length : 0
+
+      if (qtyDecimal === 0) priceStr = priceStr + '00'
+      if (qtyDecimal < 2) priceStr = priceStr + '0'
+
+      return `$ ${priceStr}`
+    }
+    return ''
+  }
+
+  useEffect(() => {
+    getGames()
+    return () => {}
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    <div className="app">
+      <header>
+        <div className="navbar">
+          <div className="name"></div>
+
+          <ul>
+            <li>Home</li>
+            <li>Games</li>
+            <li>News</li>
+            <li>About</li>
+          </ul>
+
+          <div className="avatar"></div>
+        </div>
+
+        <h1>Ninten Shop</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </header>
+
+      <main>
+        {!games.length && <h2 className="loading">loading...</h2>}
+
+        {games.length > 0 &&
+          games.map((game) => {
+            return (
+              <div className="card" key={game.objectID}>
+                <img loading="lazy" src={game.horizontalHeaderImage} alt={game.objectID} />
+                <div className="content">
+                  <span>{game.title}</span>
+                  {/* TODO: stars */}
+                  <span>{formatPrice(game.msrp)}</span>
+                </div>
+              </div>
+            )
+          })}
+      </main>
     </div>
   )
 }
