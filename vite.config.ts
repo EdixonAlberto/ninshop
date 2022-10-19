42f95bacbeafb14427d311eed4d8cad557b1c2ce
Edixon Piña <edixonalbertto@gmail.com>
2022-10-18 23:37:56 -0400

    install library to use alias in project
    - modify setting of prettier
    - add alias in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import postcssNesting from 'postcss-nesting'
+import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteAliases()],
   css: {
     postcss: {
       plugins: [postcssNesting]
